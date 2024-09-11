3ab0e15beb8f81d9f4eac694b47a060b3c61e08f
Azara <yyushahu02@gmail.com>
2024-09-11 01:28:53 +0000

    modify admin module to register classes or models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Image, Category, Photographer
 
-# Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Photographer)
