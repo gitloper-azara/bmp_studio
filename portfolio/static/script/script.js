bc33c0bf9a557c2d2d29166afe8c42173c6b1eac
Azara <yyushahu02@gmail.com>
2024-09-13 00:43:22 +0000

    add lazyloading script to load images in the user's viewport. does not work so far. return to it later

--- portfolio/static/script/script.js
+++ portfolio/static/script/script.js
@@ -2,9 +2,9 @@ const currentUrl = window.location.pathname;
 const navItems = document.querySelectorAll('.nav_items a');
 
 navItems.forEach(item => {
-    if (item.getAttribute('href') === currentUrl) {
-        item.classList.add('active');
-    }
+  if (item.getAttribute('href') === currentUrl) {
+    item.classList.add('active');
+  }
 });
 
 const menuBtn = document.getElementById('menuBtn');
@@ -12,13 +12,29 @@ const navItemsMenu = document.getElementById('navItemsMenu');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        navItemsMenu.style.display = 'block';
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        navItemsMenu.style.display = 'none';
-        menuOpen = false;
-    }
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    navItemsMenu.style.display = 'block';
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    navItemsMenu.style.display = 'none';
+    menuOpen = false;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  let lazyImages = document.querySelectorAll("img.lazy");
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let img = entry.target;
+        img.src = img.dataset.src;
+        img.classList.remove("lazy");
+        observer.unobserve(img);
+      }
+    });
+  });
+
+  lazyImages.forEach(img => observer.observe(img));
 });
