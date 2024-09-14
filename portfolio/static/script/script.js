6ac4611a40b54c4d620d345a4f3d506babff8e52
Azara <yyushahu02@gmail.com>
2024-09-14 20:14:41 +0000

    use flexbox to display images and include a back to top button to return user to top of page. write script next

--- portfolio/static/script/script.js
+++ portfolio/static/script/script.js
@@ -27,32 +27,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.image-grid');
   const items = grid.querySelectorAll('.img-container');
 
-  const resizeGridItem = (item) => {
+  const classifyImage = (item) => {
     const aspectRatio = item.dataset.aspectRatio.split('/');
-    const width = parseFloat(aspectRatio[0])
-    const height = parseFloat(aspectRatio[1])
-    const rowSpan = Math.ceil((height / width) * (item.clientWidth / 10));
-    item.style.gridRowEnd = `span ${rowSpan}`;
+    const width = parseFloat(aspectRatio[0]);
+    const height = parseFloat(aspectRatio[1]);
 
     // if an image is wider than 1.5 times its height, classify it as wide
     // if an image is taller than 1.5 times its width, classify it as tall
-    if (width / height > 1.5) {
+    if (width / height >= 1.5) {
       item.classList.add('wide');
     } else if (height / width > 1.5) {
       item.classList.add('tall');
-    } else {
-      item.classList.add('big');
     }
   };
 
-  const resizeAllGridItems = () => {
-    items.forEach(resizeGridItem);
-  };
 
-  resizeAllGridItems();
-  window.addEventListener('resize', resizeAllGridItems);
+  items.forEach(classifyImage);
 
-  const lazyImages = document.querySelectorAll('img.lazyload');
   const lazyImageObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -64,5 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  const lazyImages = document.querySelectorAll('img.lazyload');
   lazyImages.forEach(img => lazyImageObserver.observe(img));
 });
