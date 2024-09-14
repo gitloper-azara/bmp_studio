8c4b358fbe72014a9a0c73a7fe0fe8e13f2859d2
Azara <yyushahu02@gmail.com>
2024-09-14 21:52:53 +0000

    install rest_framework_simplejwt and update the settings module to reflect the installed library and the auth setting to use

--- bmp_studio/settings.py
+++ bmp_studio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'portfolio',
     'widget_tweaks',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -141,11 +142,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Rest Framework Authentication
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ],
+    ),
 }
