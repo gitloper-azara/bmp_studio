950132f1e09d0d543bf42ce13c9eab6582329a61
Azara <yyushahu02@gmail.com>
2024-10-02 19:29:34 +0000

    update top feature from text to symbol and use wildcard to match ngrok urls in allowed_hosts settings

--- bmp_studio/settings.py
+++ bmp_studio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.ngrok-free.app',]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
